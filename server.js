20472ea551dc5b6677bde547cca17bb0ef869b5d
Luke Edwards <luke.edwards05@gmail.com>
2017-11-03 15:35:58 -0700

    disable cache on `sw.js` file;
    - never cache your Service Worker!

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { h } = require('preact');
-const { join } = require('path');
 const express = require('express');
+const { join, basename } = require('path');
 const compression = require('compression')();
 const render = require('preact-render-to-string');
 const bundle = require('./build/ssr-build/ssr-bundle');
@@ -15,10 +15,15 @@ const assets = join(__dirname, 'build');
 const template = fs.readFileSync('./build/index.html', 'utf8');
 const favicon = require('serve-favicon')(join(assets, 'favicon.ico'));
 
+function setHeaders(res, file) {
+	let cache = basename(file) === 'sw.js' ? 'private,no-cache' : 'public,max-age=31536000,immutable';
+	res.setHeader('Cache-Control', cache); // disable service worker cache
+}
+
 express()
 	.use(favicon)
 	.use(compression)
-	.use(express.static(assets))
+	.use(express.static(assets, { setHeaders }))
 	.get('*', (req, res) => {
 		let url = req.url;
 		let body = render(h(App, { url }));
